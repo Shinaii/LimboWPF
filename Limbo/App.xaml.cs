f8ff8ca0cb5751474f050200da3eaf78d7d6a393
Shinaii <contact@shinaii.xyz>
2024-04-12 14:46:58 +0200

    Initial commit

---
+++ Limbo/App.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+
+namespace Limbo
+{
+    public partial class App : Application
+    {
+        public static event Action AllWindowsCreated;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            int windowID = 0;
+            base.OnStartup(e);
+            
+            int CorrectKeyID = new Random().Next(0, 7);
+
+            int numberOfWindows = 8; 
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            double windowWidth = 150; 
+            double windowHeight = 150; 
+            double spaceBetweenWindows = 50; 
+
+            double totalWidthOfFourWindows = 4 * windowWidth + 3 * spaceBetweenWindows; 
+            double totalHeightOfTwoWindows = 2 * windowHeight + spaceBetweenWindows; 
+            double centerOfScreenX = screenWidth / 2; 
+            double centerOfScreenY = screenHeight / 2; 
+
+            for (int i = 0; i < numberOfWindows; i++)
+            {
+                await Task.Delay(500); // Wait for 500ms before spawning the next window
+
+                double left;
+                double top;
+
+                if (i < 4)
+                {
+                    // For the first four windows, position them in the center of the screen
+                    left = centerOfScreenX - totalWidthOfFourWindows / 2 + i * (windowWidth + spaceBetweenWindows);
+                    top = centerOfScreenY - totalHeightOfTwoWindows / 2;
+                }
+                else
+                {
+                    // For the next four windows, position them under the first four windows
+                    left = centerOfScreenX - totalWidthOfFourWindows / 2 + (i - 4) * (windowWidth + spaceBetweenWindows);
+                    top = centerOfScreenY - totalHeightOfTwoWindows / 2 + windowHeight + spaceBetweenWindows;
+                }
+
+                MainWindow window = new MainWindow(windowID, CorrectKeyID)
+                {
+                    Left = left,
+                    Top = top
+                };
+                window.Show();
+                if (windowID == CorrectKeyID)
+                {
+                    Console.WriteLine("Key " + windowID + " spawned!" + " This is the correct key!");
+                }
+                else
+                {
+                    Console.WriteLine("Key " + windowID + " spawned!");   
+                }
+                windowID++;
+                
+                if (windowID == numberOfWindows)
+                {
+                    AllWindowsCreated?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Limbo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Limbo
+{
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private static bool isFirstWindow = true;
+        private int windowID = 0;
+        private static bool CorrectKey = false;
+        private int CorrectKeyID = 0;
+        private static bool GotClicked = false;
+        private static int totalWindows = 8;
+        private static int windowsCreated = 0;
+
+        public MainWindow(int windowID, int CorrectKeyID)
+        {
+            this.windowID = windowID;
+            this.CorrectKeyID = CorrectKeyID;
+            InitializeComponent();
+
+            idTextBlock.Text = "ID: " + windowID.ToString();
+
+            if (isFirstWindow)
+            {
+                mediaPlayer.Open(new Uri("Resource/LIMBO.mp3", UriKind.Relative));
+                mediaPlayer.Position = TimeSpan.FromSeconds(176);
+                mediaPlayer.Play();
+                isFirstWindow = false;
+            }
+
+            windowsCreated++;
+
+            if (windowID == CorrectKeyID)
+            {
+                CorrectKey = true;
+            }
+
+            App.AllWindowsCreated += StartAnimation;
+        }
+
+        private void StartAnimation()
+        {
+            if (windowID == CorrectKeyID)
+            {
+                DoubleAnimation opacityAnimation = new DoubleAnimation
+                {
+                    From = 0.0,
+                    To = 1.0,
+                    Duration = new Duration(TimeSpan.FromSeconds(0.8)),
+                    AutoReverse = true,
+                    RepeatBehavior = new RepeatBehavior(3)
+                };
+                fadeImage.BeginAnimation(OpacityProperty, opacityAnimation);
+            }
+        }
+        
+    }
+}
